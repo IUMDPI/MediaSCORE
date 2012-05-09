e2edbd5ecec95c3456c93261cb0d6c67c59bd15b
James R. Griffin III <james@avpreserve.com>
2012-05-09 05:43:26 +0000

    Applying styles.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -9,18 +9,34 @@
     <?php include_javascripts() ?>
   </head>
   <body>
-<span><a href="#">Log Out</a></span>
-<div id="content">
-	<div id="tabs">
+
+<header><div id="header" class="clearfix">
+<h1 class="ir"><a href="#">Indiana University MediaSCORE</a></h1>
+<div class="log-out"><a href="#">Log Out</a></div>
+</div></header>
+
+<nav>
+<div id="nav" class="clearfix">
+<div class="assess selected"><a href="#content-container"><img src="/images/wireframes/assess-icon.png" alt="Assess"><h2>Assess</h2></a></div>
+<div class="report"><a href="#"><img src="/images/wireframes/report-icon.png" alt="Report"><h2>Report</h2></a></div>
+<div class="settings"><a href="<?php echo url_for('storagelocation/index') ?>"><img src="/images/wireframes/settings-icon.png" alt="Settings"><h2>Settings</h2></a></div>
+</div>
+</nav>
+
+<div id="tabs">
 		<ul>
 			<li><a href="#content-container">Assess</a></li>
 			<li><a href="#">Report</a></li>
-			<li><a href="<?php echo url_for('storagelocation/index') ?>">Settings</a></li>
+			<li><a href="/frontend_dev.php/storagelocation">Settings</a></li>
 		</ul>
-		<div id="content-container">
-			<?php echo $sf_content ?>
-		</div>
+
+<div id="content">
+	<div id="content-container">
+		<?php echo $sf_content ?>
 	</div>
 </div>
-  </body>
+
+<footer><img src="/images/wireframes/indiana-university-footer-logo.png" alt="Indiana University" /><a href="#">Copyright</a> ©2008 The Trustees of <a href="#">IndianaUniversity</a> | <a href="#">Copyright Complaints</a></footer>
+
+</body>
 </html>
