4244c6745d0f172e5f9bff6bdf7b9426ea83cba0
Adeel Ahmad <adeelahmad99@gmail.com>
2012-05-25 02:18:33 +0500

    fixing symfony lib path

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,7 +1,7 @@
 <?php
 
-//require_once dirname(__FILE__).'/../lib/vendor/lib/autoload/sfCoreAutoload.class.php';
-require_once '/usr/share/pear/symfony/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__).'/../lib/vendor/lib/autoload/sfCoreAutoload.class.php';
+#require_once '/usr/share/pear/symfony/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
