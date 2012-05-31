a38c6900489588959806d36f6f0eb220666ff70f
Adeel Ahmad <adeel.ahmad@geekschicago.com>
2012-05-31 21:43:48 +0500

    format type changes and schema changes

--- lib/form/doctrine/FormatTypeForm.class.php
+++ lib/form/doctrine/FormatTypeForm.class.php
@@ -9,8 +9,10 @@
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 class FormatTypeForm extends BaseFormatTypeForm {
+    public static $durationtype=array(0=>'Calculated',1=>'estimated',2=>'max estimate');
 
     public function configure() {
+        $this->setWidget('duration_type', new sfWidgetFormChoice(array('choices'=>  self::$durationtype)));
         $this->getWidget('quantity')->setLabel('<span class="required">*</span>Quality:&nbsp;');
         $this->getWidget('generation')->setLabel('<span class="required">*</span>Generation:&nbsp;');
         $this->getWidget('year_recorded')->setLabel('<span class="required">*</span>Year Recorded:&nbsp;');
@@ -20,13 +22,16 @@ class FormatTypeForm extends BaseFormatTypeForm {
         $this->getWidget('fungus')->setLabel('Fungus:&nbsp;');
         $this->getWidget('other_contaminants')->setLabel('Other Contaminants:&nbsp;');
         $this->getWidget('duration')->setLabel('<span class="required">*</span>Duration:&nbsp;');
-        $this->getWidget('duration_type')->setLabel('Duration Type:&nbsp;');
+        $this->getWidget('duration_type')->setLabel('<span class="required">*</span>Duration Type:&nbsp;');
+        $this->getWidget('duration_type_methodology')->setLabel('<span class="required">*</span>Duration type methodology:&nbsp;');
         $this->setWidget('type', new sfWidgetFormInputHidden(array(), array('value' => 0)));
         
         $this->setValidator('quantity', new sfValidatorString(array('required' => true)));
         $this->setValidator('generation', new sfValidatorString(array('required' => true)));
         $this->setValidator('year_recorded', new sfValidatorString(array('required' => true)));
         $this->setValidator('duration', new sfValidatorString(array('required' => true)));
+        $this->setValidator('duration_type', new sfValidatorString(array('required' => true)));
+        $this->setValidator('duration_type_methodology', new sfValidatorString(array('required' => true)));
         
         $this->setWidget('generation',new sfWidgetFormChoice(array('choices' => MetalDisc::$generation)));
 
