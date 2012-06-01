643fc83ca586ae65adcd76056d616cb8c087c735
Adeel Ahmad <adeel.ahmad@geekschicago.com>
2012-06-01 20:22:29 +0500

    forgot password

---
+++ apps/frontend/modules/sfGuardAuth/templates/forgotpasswordSuccess.php
@@ -0,0 +1,25 @@
+<div>
+    <div>
+        <h3>Do not worry, we can help you get back in to your account safely!</h3>
+
+    </div>
+    <div>
+        <h4>Fill out the form below to receive an e-mail with temporary password.</h4>
+    </div>
+    <form action="<?php echo url_for('sfGuardAuth/forgotpassword') ?>" method="post">
+        <div class="row">
+            <b>Enter Email:</b>
+            <input type="input" name="email" id="email" value="<?php if(isset($email)) {echo $email;}?>"/>
+            <?php
+            if (isset($error))
+                echo '<span  class="required" style="float:none;">'.$error.'</span>';
+            ?>
+
+
+        </div>
+       
+        <div>
+            <input type="submit" value="Request Password"/>
+        </div>
+    </form>
+</div>
