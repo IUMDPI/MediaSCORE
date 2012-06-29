4a78339c9d338a6a2fb28d005f3607e23a366d1b
Adeel Ahmad <adeel.ahmad@geekschicago.com>
2012-06-29 21:28:03 +0500

    umatic issues done

--- lib/form/doctrine/UmaticForm.class.php
+++ lib/form/doctrine/UmaticForm.class.php
@@ -37,7 +37,7 @@ class UmaticForm extends BaseUmaticForm {
         $this->getWidget('pack_deformation')->setLabel('<span class="required">*</span>Pack  Deformation:&nbsp;');
 
         $this->setWidget('type', new sfWidgetFormInputHidden(array(), array('value' => $this->getObject()->getTypeValue())));
-
+$this->widgetSchema->moveField('formatVersion', 'before', 'recordingStandard');
         foreach (array('noise_reduction',
             'duration_type_methodology',
             'format_notes',
