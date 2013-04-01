25bd81d78540a66ec04e002faf7fa22ab2f3693a
Furqan <furqan@geekschicago.com>
2013-04-01 01:56:08 -0700

    Code changes

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -1,5 +1,5 @@
 <?php
-echo 'Furqan Branch';exit;
+
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
 /*if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '192.168.2.102')))
