73a81f59273986dd643989efb83ca0aaadac7da4
Furqan <furqan@geekschicago.com>
2013-04-01 02:17:38 -0700

    Code changes

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,4 @@
 <?php
-//require_once dirname(__FILE__).'/../lib/vendor/lib/autoload/sfCoreAutoload.class.php';
-//require_once 'D://xampp//htdocs//symfony//symfony//lib/autoload/sfCoreAutoload.class.php';
 require_once '/usr/share/pear/symfony/autoload/sfCoreAutoload.class.php'; 
 sfCoreAutoload::register();
 @date_default_timezone_set('America/New_York');
