87d327781b22f90cf5d3b04c9084c6e456af0a1d
Furqan <furqan@geekschicago.com>
2013-04-01 02:05:40 -0700

    Code changes

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -1,6 +1,5 @@
 <?php
-echo 'om1';
-exit;
+
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
 /*if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '192.168.2.102')))
@@ -11,4 +10,6 @@ exit;
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'testing', true);
+echo 'om1';
+exit;
 sfContext::createInstance($configuration)->dispatch();
