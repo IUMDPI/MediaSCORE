177e633bf7ad8c58ccdaa27803f28dc205580fb5
Furqan <furqan@geekschicago.com>
2013-04-01 02:14:10 -0700

    Code changes

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -1,13 +1,15 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
-/*if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '192.168.2.102')))
-{
+/* if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '192.168.2.102')))
+  {
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}*/
+  } */
 
-require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+require_once(dirname(__FILE__) . '/../config/ProjectConfiguration.class.php');
 echo 'om1221111';
 exit;
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'testing', true);
