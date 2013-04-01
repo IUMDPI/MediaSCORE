4c21358eb0290fc6f1107dcbd68c9661c3eddc1e
Furqan <furqan@geekschicago.com>
2013-04-01 02:03:12 -0700

    Code changes

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -1,5 +1,5 @@
 <?php
-echo 'om';
+echo 'om1';
 exit;
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
