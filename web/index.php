94dc63063d72c556dddbc263e47a84d3e8ecc157
Furqan <furqan@geekschicago.com>
2013-04-11 18:24:21 +0500

    bug fixed

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'testing', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
