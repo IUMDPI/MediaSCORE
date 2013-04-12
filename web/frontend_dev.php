af95c7014f07f232104c0b9a830362e392a3d185
Furqan <furqan@geekschicago.com>
2013-04-12 18:26:50 +0500

    ticket IUIPAD-204 added Recording Date Report

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -9,6 +9,6 @@
 
 require_once(dirname(__FILE__) . '/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'testing', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', true);
 
 sfContext::createInstance($configuration)->dispatch();
--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <?php
-require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'testing', false);
+require_once(dirname(__FILE__) . '/../config/ProjectConfiguration.class.php');
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
