813d3b9211f2ed86b94b5019a8c2d3f958d539d6
Furqan <furqan@geekschicago.com>
2013-04-12 18:21:12 +0500

    add tabindex for same button

--- apps/frontend/modules/characteristicsvalues/templates/_form.php
+++ apps/frontend/modules/characteristicsvalues/templates/_form.php
@@ -6,18 +6,7 @@
         <input type="hidden" name="sf_method" value="put" />
     <?php endif; ?>
     <table>
-        <tfoot>
-            <tr>
-                <td colspan="2">
-                    <?php echo $form->renderHiddenFields(false) ?>
-                    &nbsp;<a href="<?php echo url_for('characteristicsvalues/index') ?>">Back to list</a>
-                    <?php if (!$form->getObject()->isNew()): ?>
-                        &nbsp;<?php echo link_to('Delete', 'characteristicsvalues/delete?id=' . $form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
-                    <?php endif; ?>
-                    <input type="submit" value="Save" />
-                </td>
-            </tr>
-        </tfoot>
+
         <tbody>
             <?php echo $form->renderGlobalErrors() ?>
             <tr>
@@ -56,19 +45,31 @@
                 </td>
             </tr>
       <!--      <tr>
-              <th><?php // echo $form['created_at']->renderLabel()  ?></th>
+              <th><?php // echo $form['created_at']->renderLabel()   ?></th>
               <td>
             <?php // echo $form['created_at']->renderError() ?>
             <?php // echo $form['created_at'] ?>
               </td>
             </tr>
             <tr>
-              <th><?php // echo $form['updated_at']->renderLabel()  ?></th>
+              <th><?php // echo $form['updated_at']->renderLabel()   ?></th>
               <td>
             <?php // echo $form['updated_at']->renderError() ?>
             <?php // echo $form['updated_at'] ?>
               </td>
             </tr>-->
         </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="2">
+                    <?php echo $form->renderHiddenFields(false) ?>
+                    &nbsp;<a href="<?php echo url_for('characteristicsvalues/index') ?>">Back to list</a>
+                    <?php if (!$form->getObject()->isNew()): ?>
+                        &nbsp;<?php echo link_to('Delete', 'characteristicsvalues/delete?id=' . $form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
+                    <?php endif; ?>
+                    <input type="submit" value="Save" />
+                </td>
+            </tr>
+        </tfoot>
     </table>
 </form>
