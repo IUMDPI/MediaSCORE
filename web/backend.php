12e5eb3fcc7a77ebf6a1f7dd354c63a2bff60f35
Furqan <furqan@geekschicago.com>
2013-04-16 21:39:10 +0500

    IUIPAD-204 done

--- web/backend.php
+++ web/backend.php
@@ -3,5 +3,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'testing', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
--- web/backend_dev.php
+++ web/backend_dev.php
@@ -9,5 +9,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'testing', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'prod', true);
 sfContext::createInstance($configuration)->dispatch();
