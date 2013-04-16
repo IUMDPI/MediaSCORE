b35503e9db80222d9ce0fefa288ceb63ae80c54b
Furqan <furqan@geekschicago.com>
2013-04-16 21:44:14 +0500

    IUIPAD-204 done

--- web/frontend_dev.php
+++ web/frontend_dev.php
@@ -9,6 +9,6 @@
 
 require_once(dirname(__FILE__) . '/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
 
 sfContext::createInstance($configuration)->dispatch();
