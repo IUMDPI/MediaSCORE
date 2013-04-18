2ea8ef1479e337f3338737cd62a3df616a38a91c
Furqan <furqan@geekschicago.com>
2013-04-18 15:00:39 +0500

    IUIPAD - 205 ,   IUIPAD - 176  , IUIPAD - 175 ,  IUIPAD - 174 , IUIPAD - 173

--- web/index.php
+++ web/index.php
@@ -2,5 +2,5 @@
 
 require_once(dirname(__FILE__) . '/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
