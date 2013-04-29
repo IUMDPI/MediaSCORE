9ee54dfbbdedaed2ea0e52a7608fbf4c9a9dff6c
Furqan <furqan@geekschicago.com>
2013-04-29 19:12:28 +0500

    first Report Assest Group score  done

--- apps/frontend/modules/reports/templates/assetsgroupsscoringreportsSuccess.php
+++ apps/frontend/modules/reports/templates/assetsgroupsscoringreportsSuccess.php
@@ -1,6 +1,6 @@
 Recording Date Report
 
-<form action="<?php echo url_for('reports/AssetsGroupsScoringReports') ?>" method="post">
+<form action="<?php echo url_for('reports/assetsgroupsscoringreports') ?>" method="post">
     <?php echo $form['listUnits_RRD']->render(); ?>
     <?php echo $form['format_id']->render(); ?>
     <?php echo $form['ExportType']->render(); ?>
