4ac15842f7c36d35cf7b8106e8b7018eac225658
Furqan <furqan@geekschicago.com>
2013-04-29 18:45:34 +0500

    first Report Assest Group score  done

--- apps/frontend/modules/reports/templates/indexSuccess.php
+++ apps/frontend/modules/reports/templates/indexSuccess.php
@@ -2,10 +2,10 @@
 
 <ol>
     <li>
-        <h3><a href="<?php echo url_for('reports/Recordingdatereport') ?>">Recording Date Report</a></h3>
+        <h3><a href="<?php echo url_for('reports/recordingdatereport') ?>">Recording Date Report</a></h3>
     </li>
     <li>
-        <h3><a href="<?php echo url_for('reports/AssetsGroupsScoringReports') ?>">Asset Groups Scoring Reports</a></h3>
+        <h3><a href="<?php echo url_for('reports/assetsgroupsscoringreports') ?>">Asset Groups Scoring Reports</a></h3>
     </li>
 </ol>
 
