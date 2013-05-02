002d0bd898362e271c2c86cc4f7ff3a648d4a1eb
Furqan <furqan@geekschicago.com>
2013-05-02 19:04:40 +0500

    pushing report to dev for client working of all data reports 4 reports are done

--- apps/frontend/modules/twoinchopenreelvideo/templates/_form.php
+++ apps/frontend/modules/twoinchopenreelvideo/templates/_form.php
@@ -1,17 +1,17 @@
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
 
-<form action="<?php echo url_for('twoinchopenreelvideo/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
-<?php if (!$form->getObject()->isNew()): ?>
-<input type="hidden" name="sf_method" value="put" />
-<?php endif; ?>
-  <table>
-    <tfoot>
-    </tfoot>
-    <tbody>
-      <?php echo $form ?>
-    </tbody>
-  </table>
+<form action="<?php echo url_for('twoinchopenreelvideo/' . ($form->getObject()->isNew() ? 'create' : 'update') . (!$form->getObject()->isNew() ? '?id=' . $form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
+    <?php if (!$form->getObject()->isNew()): ?>
+        <input type="hidden" name="sf_method" value="put" />
+    <?php endif; ?>
+    <table>
+        <tfoot>
+        </tfoot>
+        <tbody>
+            <?php echo $form ?>
+        </tbody>
+    </table>
 </form>
 <script type="text/javascript">
     $(function(){
@@ -24,6 +24,6 @@
 
 <script type="text/javascript">
     $(function(){
-        $("#format_type_off_brand").parents(".row").show();
+        $("#format_type_fungus").parents(".row").show();
     });
 </script>
