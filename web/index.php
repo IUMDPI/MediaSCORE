c6f11b41fddac819c02ad3c740dbf72fbbf14de4
Nouman Tayyan <noumantayyab@gmail.com>
2013-05-21 22:19:02 +0500

    configuration changes

--- web/index.php
+++ web/index.php
@@ -2,17 +2,11 @@
 
 $serverName = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : gethostname();
 if (strpos($serverName, 'mediascore.avpreserve.com') !== FALSE)
-{
 	define('ENVIRONMENT', 'production');
-}
 else if (strpos($serverName, '108.166.74.254') !== FALSE)
-{
 	define('ENVIRONMENT', 'qa');
-}
 else
-{
 	define('ENVIRONMENT', 'local');
-}
 require_once(dirname(__FILE__) . '/../config/ProjectConfiguration.class.php');
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', ENVIRONMENT, FALSE);
 sfContext::createInstance($configuration)->dispatch();
