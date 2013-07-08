b1a4b72468c8f4ae0ba474dbf56237e3384ddbaa
Nouman Tayyab <noumantayyab@gmail.com>
2013-07-08 19:23:52 +0500

    report scoring changes

--- apps/frontend/modules/reports/templates/indexSuccess.php
+++ apps/frontend/modules/reports/templates/indexSuccess.php
@@ -1,42 +1,32 @@
-<h1>Reports List</h1>
+<div><h1>Reports List</h1>
+</div>
 <table>
 	<tbody>
 		<tr>
 			<th><label><a href="<?php echo url_for('reports/recordingdatereport') ?>">Recording Date Report</a></label></th>
 		</tr>
+		<tr>
+			<th><label><a href="<?php echo url_for('reports/assetsgroupsscoringreports') ?>">Asset Groups Scoring Reports</a></label></th>
+		</tr>
+		<tr>
+			<th><label><a href="<?php echo url_for('reports/collectionstatusreport') ?>">Collection Status Report</a></label></th>
+		</tr>
+		<tr>
+			<th><label><a href="<?php echo url_for('reports/problemmediareport') ?>">Problem Media Report</a></label></th>
+		</tr>
+		<tr>
+			<th><label><a href="<?php echo url_for('reports/alldataoutputreport') ?>">All Data output</a></label></th>
+		</tr>
+		<tr>
+			<th><label><a href="<?php echo url_for('reports/evaluatorsreport') ?>">Evaluator's Report</a></label></th>
+		</tr>
+		<tr>
+			<th><label><a href="<?php echo url_for('reports/percentageofholdings') ?>">Percentage of holdings</a></label></th>
+		</tr>
+		<tr>
+			<th><label><a href="<?php echo url_for('reports/durationandquantitysearch') ?>">Duration Reports</a></label></th>
+		</tr>
 	</tbody>
 </table>
-<ul>
-    <li>
-        <h3><a href="<?php echo url_for('reports/recordingdatereport') ?>">Recording Date Report</a></h3>
-    </li>
 
-    <li>
-        <h3><a href="<?php echo url_for('reports/assetsgroupsscoringreports') ?>">Asset Groups Scoring Reports</a></h3>
-    </li>
-
-    <li>
-        <h3><a href="<?php echo url_for('reports/collectionstatusreport') ?>">Collection Status Report</a></h3>
-    </li>
-
-    <li>
-        <h3><a href="<?php echo url_for('reports/problemmediareport') ?>">Problem Media Report</a></h3>
-    </li>
-
-    <li>
-        <h3><a href="<?php echo url_for('reports/alldataoutputreport') ?>">All Data output</a></h3>
-    </li>
-
-    <li>
-        <h3><a href="<?php echo url_for('reports/evaluatorsreport') ?>">Evaluator's Report</a></h3>
-    </li>
-
-    <li>
-        <h3><a href="<?php echo url_for('reports/percentageofholdings') ?>">Percentage of holdings</a></h3>
-    </li>
-
-    <li>
-        <h3><a href="<?php echo url_for('reports/durationandquantitysearch') ?>">Duration Reports</a></h3>
-    </li>
-</ul>
 
