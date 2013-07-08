c4fa05a08cdba7248670760999875fa1d21dea5b
Nouman Tayyab <noumantayyab@gmail.com>
2013-07-08 13:37:55 +0500

    report scoring changes

--- apps/frontend/modules/reports/templates/assetsgroupsscoringreportsSuccess.php
+++ apps/frontend/modules/reports/templates/assetsgroupsscoringreportsSuccess.php
@@ -40,13 +40,21 @@ echo $NoRecordFound = get_slot('my_slot');
         'height':200
         
     });
-    
+    $("#reports_listUnits_RRD").bind("multiselectclick", function(event, ui){
+            var array_of_checked_values = $("#reports_listUnits_RRD").multiselect("getChecked").map(function(){
+                return this.value;	
+            }).get();
+        
+//            if(array_of_checked_values!='')
+                getUnitFormat(array_of_checked_values);
+            
+        });
     $('#reports_collectionStatus').multiselect({
         'height':'auto',
         'multiple':true,
         'height':200
     });
-	function getUnitFormat(){
-		alert(1);
+	function getUnitFormat(ids){
+		console.log(ids)
 	}
 </script>
