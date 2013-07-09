d4b9b10ab473bb69998ba424c457d866474c6e0e
Nouman Tayyab <noumantayyab@gmail.com>
2013-07-09 15:01:57 +0500

    reporting changes

--- apps/frontend/modules/reports/templates/problemmediareportSuccess.php
+++ apps/frontend/modules/reports/templates/problemmediareportSuccess.php
@@ -1,60 +1,64 @@
-Problem  Media Report 
-<br/>
-<br/>
+<h1>Problem  Media Report</h1>
+<hr/>
 <?php
 echo $NoRecordFound = get_slot('my_slot');
 ?>
 
 <form action="<?php echo url_for('reports/problemmediareport') ?>" method="post">
-    <?php echo $form['listCollection_RRD']->renderLabel(); ?>
-    <br/>
-    <br/>
-    <?php echo $form['listCollection_RRD']->render(); ?>
-    <br/>
-    <br/>
-    <br/>
-    <?php echo $form['Constraints']->renderLabel(); ?>
-    <br/>
-    <br/>
-    <?php echo $form['Constraints']->render(); ?>
-    <br/>
-    <br/>
-    <br/>
+	<table>
 
-    <?php echo $form['ExportType']->renderLabel(); ?>
-    <br/>
-    <br/>
-    <?php echo $form['ExportType']->render(); ?>
-    <br/>
-    <br/>
-    <br/>
-    <input type="submit" value="Export" />
+		<tfoot>
+            <tr>
+                <td colspan="2">
+					<input type="submit" value="Export" />&nbsp;<span>or</span>&nbsp;<a href="<?php echo url_for('reports/index') ?>">Cancel</a>
+                </td>
+            </tr>
+        </tfoot>
+		<tbody>
+			<tr>
+                <th>
+					<?php echo $form->renderHiddenFields(); ?>
+					<?php echo $form['listCollection_RRD']->renderLabel(); ?>
+                </th>
+                <td>
+					<?php echo $form['listCollection_RRD']->render(); ?>
+					<?php echo $form['listCollection_RRD']->renderError(); ?>
+                </td>
+            </tr>
+			<tr>
+                <th>
 
+					<?php echo $form['Constraints']->renderLabel(); ?>
+                </th>
+                <td>
+					<?php echo $form['Constraints']->render(); ?>
+					<?php echo $form['Constraints']->renderError(); ?>
+                </td>
+            </tr>
+			<tr>
+                <th>
 
+					<?php echo $form['ExportType']->renderLabel(); ?>
+                </th>
+                <td>
+					<?php echo $form['ExportType']->render(); ?>
+					<?php echo $form['ExportType']->renderError(); ?>
+                </td>
+            </tr>
+		</tbody>
+	</table>
 </form>
 
 <script type="text/javascript">
-    $('#reports_listUnits_RRD').multiselect({
-        'height':'auto',
-        'multiple':true,
-        'height':200
-    });
-    $('#reports_listCollection_RRD').multiselect({
-        'height':'auto',
-        'multiple':true,
-        'height':200
-        
-    });
-    
-    $('#reports_collectionStatus').multiselect({
-        'height':'auto',
-        'multiple':true,
-        'height':200
-    });
-    $('#reports_Constraints').multiselect({
-        'height':'auto',
-        'multiple':true,
-        'height':200
-    });
-  
+
+	$('#reports_listCollection_RRD').multiselect({
+		multiple: true,
+		height: 200
+
+	});
+	$('#reports_Constraints').multiselect({
+		height: 'auto',
+		multiple: true,
+	});
+
 </script>
