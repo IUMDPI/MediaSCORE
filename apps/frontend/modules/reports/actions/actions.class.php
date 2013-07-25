66c2ea28e2648d5aa68f3a6bd4367ca5487e5201
Nouman Tayyab <noumantayyab@gmail.com>
2013-07-25 16:28:30 +0500

    changes

--- apps/frontend/modules/reports/actions/actions.class.php
+++ apps/frontend/modules/reports/actions/actions.class.php
@@ -693,7 +693,7 @@ class reportsActions extends sfActions
 							if ($AssetScoreReport['Score'] != '')
 								$AssetScoreReportArray[] = $AssetScoreReport;
 						}
-						
+						echo '<pre>';print_r($AssetScoreReportArray);exit;
 //						$AssetScoreReportArray = $commonFunctions->arsort($AssetScoreReportArray, 'Year Recorded');
 
 						if ($ExportType == 'xls')
