3a68541e838941da90133eb0db770693398f830a
Nouman Tayyab <noumantayyab@gmail.com>
2013-09-25 17:12:35 +0500

    features and bugs

--- apps/frontend/modules/characteristicsvalues/templates/_form.php
+++ apps/frontend/modules/characteristicsvalues/templates/_form.php
@@ -57,9 +57,9 @@
                 <td colspan="2">
 
                     &nbsp;<a href="<?php echo url_for('characteristicsvalues/index') ?>">Back to list</a>
-					<?php if ( ! $form->getObject()->isNew()): ?>
-						&nbsp;<?php echo link_to('Delete', 'characteristicsvalues/delete?id=' . $form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
-					<?php endif; ?>
+					<?php // if ( ! $form->getObject()->isNew()): 
+						 //echo link_to('Delete', 'characteristicsvalues/delete?id=' . $form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ;
+					 //endif; ?>
                     <input type="submit" value="Save" />
                 </td>
             </tr>
