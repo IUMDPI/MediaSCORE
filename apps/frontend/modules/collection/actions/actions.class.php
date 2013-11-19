bb3c5b1f74ceb04e8948f85d615ed79b206ba8d2
Nouman Tayyab <noumantayyab@gmail.com>
2013-11-20 00:14:21 +0500

    furqan bug fixes

--- apps/frontend/modules/collection/actions/actions.class.php
+++ apps/frontend/modules/collection/actions/actions.class.php
@@ -375,6 +375,7 @@ class collectionActions extends sfActions {
                     ->where('id =?', $unitId)
                     ->execute();
 //            header('location: ' . $this->generateUrl("collection", $unit[0]));
+			echo $this->generateUrl("collection", $unit);exit;
             $this->redirect($this->generateUrl("collection", $unit));
             echo '<script>' . $this->generateUrl("collection", $unit) . '</script>';
             exit;
