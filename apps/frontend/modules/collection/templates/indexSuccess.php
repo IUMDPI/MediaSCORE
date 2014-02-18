03dcee49aa62369c9ae335cb3202499fbe30bd77
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-18 12:56:30 +0500

    issue debuging

--- apps/frontend/modules/collection/templates/indexSuccess.php
+++ apps/frontend/modules/collection/templates/indexSuccess.php
@@ -646,3 +646,10 @@ if (sizeof($collections) > 0)
 		</div>
 	</div>
 <?php } ?>
+	<?php
+		$end_time = microtime(TRUE);
+		$time_taken = $end_time - $start_time;
+		$time_taken = round($time_taken, 5);
+
+		echo 'Page generated in ' . $time_taken . ' seconds.';
+		?>
