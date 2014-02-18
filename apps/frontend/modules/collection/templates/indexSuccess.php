017353e54b7dbd58503a4c596e8b65d057b73fa4
Nouman Tayyab <noumantayyab@gmail.com>
2014-02-18 12:38:03 +0500

    issue debuging

--- apps/frontend/modules/collection/templates/indexSuccess.php
+++ apps/frontend/modules/collection/templates/indexSuccess.php
@@ -2,6 +2,7 @@
 //@set_time_limit(0);
 //@ini_set("memory_limit", "1000M"); # 1GB
 //@ini_set("max_execution_time", 999999999999); # 1GB
+$start_time = microtime(TRUE);
 if ( ! isset($view) || $view == '')
 	$view = 'score';
 if ($sf_user->getGuardUser()->getRole() != 2 || $view == 'river')
@@ -334,9 +335,7 @@ if ($view == 'river')
 										}
 										?>
 
-										<td <?php echo ($morethenlengthInstId) ? 'class="long_name_handler_inst tooltip"' : 'class="long_name_handler_inst"'; ?>><a href="<?php echo url_for('assetgroup', $collection) ?>"><?php
-												echo $alterInstId;
-												?> <span><?php echo ($morethenlengthInstId) ? $getInstId : ''; ?> </span></a></td>
+											<td> <span><?php echo ($morethenlengthInstId) ? $getInstId : ''; ?> </span></a></td>
 										<?php
 										$getName = $collection->getName();
 										$lenthName = strlen($getName);
@@ -350,10 +349,7 @@ if ($view == 'river')
 											$morethenlengthName = TRUE;
 										}
 										?>
-										<td <?php echo ($morethenlengthName) ? 'class="long_name_handler tooltip"' : 'class="long_name_handler"'; ?> ><a href="<?php echo url_for('assetgroup', $collection) ?>">
-												<?php
-												echo $alterName;
-												?> <span><?php echo ($morethenlengthName) ? $getName : ''; ?></span></a></td>
+										<td><span><?php echo ($morethenlengthName) ? $getName : ''; ?></span></a></td>
 
 										<td width="9%"><?php echo date('Y-m-d', strtotime($collection->getCreatedAt())); ?></td>
 										<td><span style="display: none;"><?php echo $collection->getCreator()->getLastName() ?></span><?php echo $collection->getCreator()->getName() ?></td>
@@ -645,3 +641,10 @@ if (sizeof($collections) > 0)
 		</div>
 	</div>
 <?php } ?>
+<?php
+		$end_time = microtime(TRUE);
+		$time_taken = $end_time - $start_time;
+		$time_taken = round($time_taken, 5);
+
+		echo 'Page generated in ' . $time_taken . ' seconds.';
+		?>
