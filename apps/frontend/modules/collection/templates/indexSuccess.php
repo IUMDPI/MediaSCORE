afa0e7ffbd4386ea02415efcb5ba9e34e770e9bf
Nouman Tayyab <noumantayyab@gmail.com>
2014-03-08 01:08:06 +0500

    removed popup html

--- apps/frontend/modules/collection/templates/indexSuccess.php
+++ apps/frontend/modules/collection/templates/indexSuccess.php
@@ -1,6 +1,6 @@
 <?php
 @set_time_limit(0);
-@ini_set("memory_limit", "4000M"); # 3GB
+//@ini_set("memory_limit", "4000M"); # 3GB
 @ini_set("max_execution_time", 999999999999); # 1GB
 if ( ! isset($view) || $view == '')
 	$view = 'score';
