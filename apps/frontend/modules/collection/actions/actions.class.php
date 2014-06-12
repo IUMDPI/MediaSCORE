048d275de4154ff85ec61b638f0a04a3900e7540
Nouman Tayyab <noumantayyab@gmail.com>
2014-06-12 17:19:56 +0500

    import changes

--- apps/frontend/modules/collection/actions/actions.class.php
+++ apps/frontend/modules/collection/actions/actions.class.php
@@ -604,7 +604,7 @@ class collectionActions extends sfActions
 			$collection = Doctrine_Query::Create()
 				->from('Collection c')
 //				->select('c.*')
-				->where('c.name = ', $row[2])
+				->where('name = ', $row[2])
 				->fetchOne();
 //			if ( ! $collection)
 //			{
