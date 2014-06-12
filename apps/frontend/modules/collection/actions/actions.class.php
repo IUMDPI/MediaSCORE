c97df744b095f5802743391f340e2445ea74799d
Nouman Tayyab <noumantayyab@gmail.com>
2014-06-12 19:20:12 +0500

    changes

--- apps/frontend/modules/collection/actions/actions.class.php
+++ apps/frontend/modules/collection/actions/actions.class.php
@@ -591,7 +591,7 @@ class collectionActions extends sfActions
 				$collection = Doctrine_Query::Create()
 					->from('Collection c')
 //				->select('c.*')
-					->where('c.name = ?', $title)
+					->where('c.name LIKE ?', $title)
 					->fetchOne();
 				if ( ! $collection)
 				{
