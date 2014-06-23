cb60ddf7320e4ca18170ada7d439012577e5c31d
Nouman Tayyab <noumantayyab@gmail.com>
2014-06-23 15:18:09 +0500

    debugging report issue

--- apps/frontend/modules/reports/actions/actions.class.php
+++ apps/frontend/modules/reports/actions/actions.class.php
@@ -1203,11 +1203,11 @@ class reportsActions extends sfActions
 
 								if ($formatTypeValuesManager->getArrayOfValueTargeted($asset['FormatType']['type'], 'formatVersion', $formatValue))
 								{
-									$format .= $formatTypeValuesManager->getArrayOfValueTargeted($asset['FormatType']['type'], 'formatVersion', str_replace('±', '&#177;', $formatValue)) . ' , ';
+									$format .= $formatTypeValuesManager->getArrayOfValueTargeted($asset['FormatType']['type'], 'formatVersion', str_replace('±', '', $formatValue)) . ' , ';
 								}
 								else
 								{
-									$format .= $formatTypeValuesManager->getArrayOfValueTargeted('general', 'formatVersion', str_replace('±', '&#177;', $formatValue)) . ' , ';
+									$format .= $formatTypeValuesManager->getArrayOfValueTargeted('general', 'formatVersion', str_replace('±', '', $formatValue)) . ' , ';
 								}
 							}
 
